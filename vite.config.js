51d543db517a11c63cdfb9a4b2da3932794b1b14
Caio Cohen <caio.cohen@epicor.com>
2025-08-15 17:19:45 -0300

    build: publish to docs for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/caioCohen/',          // nome exato do repositório
+  build: { outDir: 'docs' }     // gera o build dentro de /docs
 })
